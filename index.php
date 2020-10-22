f7a906f9ad7050159031c870135322c411f4d10a
aisteRutkauskaite <aiste924@gmail.com>
2020-10-22 11:08:37 +0300

    $square_color

--- index.php
+++ index.php
@@ -1,2 +1,29 @@
 <?php
-print "veikia";
+define('G', 0);
+define('Y', 1);
+define('R', 2);
+
+$square_color = [
+    [G, G, G, G, G, G, G, G],
+    [G, Y, Y, Y, Y, Y, Y, G],
+    [G, Y, R, R, R, R, Y, G],
+    [G, Y, R, Y, Y, R, Y, G],
+    [G, Y, R, Y, Y, R, Y, G],
+    [G, Y, R, R, R, R, Y, G],
+    [G, Y, Y, Y, Y, Y, Y, G],
+    [G, G, G, G, G, G, G, G]
+];
+
+var_dump($square_color );
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title> kazkas </title>
+
+</head>
+<body>
+
+</body>
+</html>
