09142a71722235c195a6c532defa05168ca79f2e
aisteRutkauskaite <aiste924@gmail.com>
2020-10-26 11:58:54 +0200

    police report

--- index.php
+++ index.php
@@ -1,61 +1,43 @@
 <?php
-
-$users = [
+$police_report = [
     [
-        'user_name' => 'Luk Skywalker',
-        'user_email' => 'LukSkywalker@gmail.com',
-        'user_image' => 'https://static.wikia.nocookie.net/battlefront/images/4/4a/Battlefront_Luke.jpg/revision/latest/top-crop/width/360/height/360?cb=20151022170630',
+        'subject' => 'Minde',
+        'reason' => 'Was not at the party',
+        'amount' => rand(-20, 50),
     ],
     [
-        'user_name' => 'Darth Vader',
-        'user_email' => 'DarthVader@gmail.com',
-        'user_image' => 'https://i.pinimg.com/originals/c1/27/f3/c127f31130e49eb606ebf9a9be204c4c.jpg',
+        'subject' => 'Sigutis',
+        'reason' => 'Steal computer',
+        'amount' => rand(-20, 50),
     ],
     [
-        'user_name' => 'Chewbacca',
-        'user_email' => 'Chewbacca@gmail.com',
-        'user_image' => 'https://www.lrt.lt/img/2019/05/03/420817-663811-1287x836.jpg',
-    ],
-    [
-        'user_name' => 'Han Solo',
-        'user_email' => 'Solo@gmail.com',
-        'user_image' => 'https://s29588.pcdn.co/wp-content/uploads/sites/2/2018/04/Star-Wars-Han-Solo-Leather-Vest-Jacket1.png',
-    ],
+        'subject' => 'Agnė',
+        'reason' => 'Go somewhere on friday too early',
+        'amount' => rand(-20, 50),
+    ]
 ];
 
+foreach ($police_report as $index => $report) {
+    $police_report[$index]['warning_only'] = rand(0, 1) ? true : false;
+    if ($report['amount'] < 0) {
+        $police_report[$index]['css_class'] = 'expense';
+    } else {
+        $police_report[$index]['css_class'] = 'income';
+    }
+}
 
+var_dump($police_report);
 ?>
-<!DOCTYPE html>
+
+<!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Cards</title>
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Bloopers</title>
 </head>
-<style>
-    body {
-        display: flex;
-    }
-
-    img {
-        height: 200px;
-        width: 100%;
-    }
-
-    div {
-        border: 1px solid grey;
-        text-align: center;
-        width: 200px;
-    }
-</style>
 <body>
-<?php foreach ($users as $value): ?>
-    <div>
-        <img src="<?php print $value['user_image']; ?>" alt="photo">
-        <div>
-            <h2><?php print $value['user_name']; ?></h2>
-            <p><?php print $value['user_email']; ?></p>
-        </div>
-    </div>
-<?php endforeach; ?>
 </body>
 </html>
