c328d050a573ffd79229ad216d4b074a27e8e464
aisteRutkauskaite <aiste924@gmail.com>
2020-10-30 10:44:05 +0200

    references

--- index.php
+++ index.php
@@ -1,34 +1,17 @@
 <?php
+$street = [];
 
-function generate_matrix($size)
-{
-    $generated_array = [];
-    for ($i = 0; $i < $size; $i++) {
-        for ($j = 0; $j < $size; $j++) {
-            $generated_array[$i][$j] = rand(0, 1);
-        }
-    }
-    return $generated_array;
+for ($x = 0; $x < 5; $x++) {
+    $street[] = 'gražus namas su elektra';
+}
+var_dump($street);
+
+foreach ($street as $key => $house) {
+    $street[$key] = &$house;
 }
 
-$matrix = generate_matrix(rand(2, 4));
-//var_dump($matrix);
-
-function get_wining_rows($array)
-{
-    $generated_new_array = [];
-    foreach ($array as $index => $numbers) {
-        if (count(array_unique($numbers)) === 1) {
-            $generated_new_array[] = $index;
-        } else {
-            print 'Not';
-        }
-    }
-    return $generated_new_array;
-}
-
-$wining_rows = get_wining_rows($matrix);
-var_dump($wining_rows);
+$street[2] = 'dingo elektra';
+var_dump($street);
 
 ?>
 
@@ -41,48 +24,7 @@ var_dump($wining_rows);
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Functions</title>
 </head>
-<style>
-    * {
-        box-sizing: border-box;
-    }
-
-    body {
-        display: flex;
-        flex-direction: column;
-        margin: 0 auto;
-    }
-
-    .container {
-        display: flex;
-    }
-
-    .yellow {
-        width: 100px;
-        height: 100px;
-        background-color: yellow;
-        margin: 10px;
-    }
-
-    .blue {
-        width: 100px;
-        height: 100px;
-        background-color: cornflowerblue;
-        margin: 10px;
-    }
-
-    .line {
-        border: 1px solid red;
-        width: fit-content;
-    }
-
-</style>
 <body>
-<?php foreach ($matrix as $index => $row): ?>
-    <div class="container <?php print in_array($index, $wining_rows) ? 'line' : ''; ?>  ">
-        <?php foreach ($row as $col): ?>
-            <span class="<?php print $col ? 'blue' : 'yellow'; ?>"></span>
-        <?php endforeach; ?>
-    </div>
-<?php endforeach; ?>
+
 </body>
 </html>
