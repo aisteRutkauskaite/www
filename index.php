290a833d2ba501ca03db113fa01273077253715a
aisteRutkauskaite <aiste924@gmail.com>
2020-11-04 10:29:58 +0200

    form+attributai

--- index.php
+++ index.php
@@ -1,30 +1,34 @@
 <?php
-var_dump($_POST);
-$email = isset($_POST['email']) ? ($_POST['email']) : '';
+$email = isset($_POST['email']) ? $_POST['email'] : '';
 $form = [
     'email' => [
-        'label' => 'Email:',
+        'label' => 'Email',
         'type' => 'text',
-        'placeholder' => 'Aurimas:'
+        'placeholder' => 'aiste@gmail.com'
     ],
     'password' => [
-        'label' => 'Password:',
+        'label' => 'Password',
         'type' => 'password',
-        'placeholder' => 'Your password'
+        'placeholder' => 'Your password...'
     ]
 ];
-
+function html_attr(array $attr): string {
+    $string = '';
+    foreach ($attr as $name => $value) {
+        $string .= "$name=\"$value\" ";
+    }
+    return $string;
+}
+var_dump(html_attr($form['email']));
 function get_clean_input($form) {
     $parameters = [];
-    foreach ($form as $index => $item) {
+    foreach ($form as $index => $input) {
         $parameters[$index] = FILTER_SANITIZE_SPECIAL_CHARS;
     }
     return filter_input_array(INPUT_POST, $parameters);
 }
-
-var_dump( get_clean_input($form));
+$svarus_inputai = get_clean_input($form);
 ?>
-
 <!doctype html>
 <html lang="en">
 <head>
@@ -32,32 +36,19 @@ var_dump( get_clean_input($form));
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Functions</title>
+    <link rel="stylesheet" href="style.css">
+    <title>Forma</title>
 </head>
-<style>
-    form {
-        display: flex;
-        flex-direction: column;
-    }
-
-    .button {
-        width: 100px;
-    }
-</style>
 <body>
 <main>
-
-    <form method=post>
+    <form method="post">
         <?php foreach ($form as $input_name => $input): ?>
-            <label for="">
-                <?php print $input['label']; ?>
-                <input type="<?php print $input['type']; ?> name="<?php print $input_name; ?> "
-                placeholder="<?php print $input['placeholder']; ?>">
+            <label><?php print $input['label']; ?>
+                <input <?php print html_attr($input); ?>>
             </label>
         <?php endforeach; ?>
-        <button name="button">Log in</button>
+        <button name="button">Login</button>
     </form>
-
 </main>
 </body>
 </html>
